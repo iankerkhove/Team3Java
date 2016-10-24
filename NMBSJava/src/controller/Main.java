613c2f4f955dcc6ac222516bc295d103ad4397ab
Jonas DF <defrere.jonas@gmail.com>
2016-10-24 11:37:06 +0200

    Nieuwe funtie voor Route op basis van tijd en specifieke trien opvragen

--- NMBSJava/src/controller/Main.java
+++ NMBSJava/src/controller/Main.java
@@ -1,23 +1,10 @@
 package controller;
 
-import model.Routeberekening;;
+import model.Routeberekening;
+import model.Train;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		Routeberekening r = new Routeberekening("Brussel-Zuid", "Geraardsbergen");
-		
-		// krijg van/naar terug
-		System.out.println("Van: " + r.getStepOn() + "\nNaar: " + r.getStepOff());
-		
-		// krijg ID van de eerste trein van de eerste route
-		System.out.println(r.getRoutes().get(0).getTrains().get(0).getFullId());
-		
-		// krijg geplande vertrektijd van eerste trein van de eerste route
-		System.out.println(r.getRoutes().get(0).getTrains().get(0).getTime().getDeparture());
-		
-		// krijg aantal te nemen treinen tijdens de eerste route
-		System.out.println(r.getRoutes().get(0).getTrains().size());
 	}
 }
