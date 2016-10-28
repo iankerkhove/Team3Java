42119a4156fddfea0f3dfb688cc1f64cf3180873
Jonas DF <defrere.jonas@gmail.com>
2016-10-28 19:01:28 +0200

    voorbeeldmain

--- NMBSJava/src/controller/Main.java
+++ NMBSJava/src/controller/Main.java
@@ -1,16 +1,21 @@
 package controller;
 
 import model.RouteberekeningAPI;
+import model.StationboardAPI;
+import model.TrainAPI;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		try {
-			RouteberekeningAPI.readUrl("https://traintracks.online/api/Route/Geraardsbergen/Brussel-Zuid");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+//			StationboardAPI s = new StationboardAPI("Brussel-Zuid");
+//			System.out.println(s.getTrains().get(0).getFullId());
+//			
+//			RouteberekeningAPI r = new RouteberekeningAPI("Brussel-Zuid", "Oostende");
+//			System.out.println(r.getRoutes().get(0).getTrains().get(0).getFullId());
+//			
+//			TrainAPI t = new TrainAPI("1717");
+//			System.out.println(t.getFullId());
+			
 	}
 }
