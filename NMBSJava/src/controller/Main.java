21fad8ca40e0e270426398369589348e008977e3
Jonas DF <defrere.jonas@gmail.com>
2016-11-01 15:08:34 +0100

    JDatePicker: implementatie voor gemakkelijke selectie van datum
    LGoodDatePicker: implementatie voor selectie van tijd

--- NMBSJava/src/controller/Main.java
+++ NMBSJava/src/controller/Main.java
@@ -1,8 +1,25 @@
 package controller;
 
+import java.awt.EventQueue;
+
+import gui.GUIFrame;
+import gui.RouteberekeningPanel;
+
 public class Main {
 
 	public static void main(String[] args) {
-			
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIFrame frame = new GUIFrame();
+					frame.setVisible(true);
+					RouteberekeningPanel pane = new RouteberekeningPanel();
+					pane.setVisible(true);
+					frame.getContentPane().add(pane);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
--- NMBSJava/src/gui/RouteberekeningPanel.java
+++ NMBSJava/src/gui/RouteberekeningPanel.java
@@ -1,11 +1,117 @@
 package gui;
 
-import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.util.Properties;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import org.jdatepicker.impl.*;
+
+import com.github.lgooddatepicker.components.TimePicker;
+
+import java.awt.Font;
+
+@SuppressWarnings("serial")
 public class RouteberekeningPanel extends JPanel {
-	
+
+	private JPanel searchPanel;
+	private JPanel timePanel;
+	private JPanel resultPanel;
+
+	private JLabel lblTitle;
+	private JLabel lblVan;
+	private JLabel lblNaar;
+	private JLabel lblTijd;
+	private JLabel lblResult;
+
+	private JTextField txtVan;
+	private JTextField txtNaar;
+	private JDatePickerImpl datePicker;
+	private TimePicker timePicker;
+
+	private JButton btnZoek;
+
 	public RouteberekeningPanel() {
-		
+		initSearchPanel();
+		initResultPanel();
+		initializeCompletePanel();
 	}
 
+	private void initializeCompletePanel() {
+		/* init pane */
+		this.setLayout(new GridLayout(2, 1, 5, 5));
+		this.setVisible(false);
+
+		/* Add all components */
+		this.add(searchPanel);
+		this.add(resultPanel);
+	}
+
+	private void initResultPanel() {
+		/* init pane */
+		resultPanel = new JPanel();
+		resultPanel.setLayout(new GridLayout(1, 1, 5, 5));
+
+		/* Create all components */
+		lblResult = new JLabel("Druk op zoeken om een route weer te geven.");
+		lblResult.setHorizontalAlignment(SwingConstants.LEFT);
+		lblResult.setVerticalAlignment(SwingConstants.TOP);
+
+		/* Add all components */
+		resultPanel.add(lblResult);
+
+	}
+
+	private void initSearchPanel() {
+		/* init pane */
+		searchPanel = new JPanel();
+		searchPanel.setLayout(new GridLayout(5, 2, 5, 5));
+
+		/* Create all components */
+		lblTitle = new JLabel("Routeberekening");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
+		lblVan = new JLabel("Van: ");
+		txtVan = new JTextField("van");
+
+		lblNaar = new JLabel("Naar: ");
+		txtNaar = new JTextField("naar");
+
+		lblTijd = new JLabel("Tijd:");
+		{
+			timePanel = new JPanel();
+			timePanel.setLayout(new GridLayout(1, 2, 5, 5));
+			
+			Properties properties = new Properties();
+			properties.put("text.today", "Today");
+			properties.put("text.month", "Month");
+			properties.put("text.year", "Year");
+			JDatePanelImpl datePanel = new JDatePanelImpl(new UtilDateModel(), properties);
+			datePicker = new JDatePickerImpl(datePanel, new GUIDateFormat());
+			datePicker.getJFormattedTextField().setText("01/11/2016");
+			timePicker = new TimePicker();
+			timePicker.setText("12:00");
+			
+			timePanel.add(datePicker);
+			timePanel.add(timePicker);
+		}
+
+		btnZoek = new JButton("Zoek");
+
+		/* Add all components */
+		searchPanel.add(lblTitle);
+		searchPanel.add(new JLabel());
+		searchPanel.add(lblVan);
+		searchPanel.add(txtVan);
+		searchPanel.add(lblNaar);
+		searchPanel.add(txtNaar);
+		searchPanel.add(lblTijd);
+		searchPanel.add(timePanel);
+		searchPanel.add(new JLabel());
+		searchPanel.add(btnZoek);
+	}
 }
