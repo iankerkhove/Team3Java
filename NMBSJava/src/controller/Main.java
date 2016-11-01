9b8b679a4bb944554416fb99a5b09af3f64d2372
Jonas DF <defrere.jonas@gmail.com>
2016-11-01 11:43:19 +0100

    clear Main for testing

--- NMBSJava/src/controller/Main.java
+++ NMBSJava/src/controller/Main.java
@@ -1,21 +1,8 @@
 package controller;
 
-import model.RouteberekeningAPI;
-import model.StationboardAPI;
-import model.TrainAPI;
-
 public class Main {
 
 	public static void main(String[] args) {
-
-//			StationboardAPI s = new StationboardAPI("Brussel-Zuid");
-//			System.out.println(s.getTrains().get(0).getFullId());
-//			
-//			RouteberekeningAPI r = new RouteberekeningAPI("Brussel-Zuid", "Oostende");
-//			System.out.println(r.getRoutes().get(0).getTrains().get(0).getFullId());
-//			
-//			TrainAPI t = new TrainAPI("1717");
-//			System.out.println(t.getFullId());
 			
 	}
 }
