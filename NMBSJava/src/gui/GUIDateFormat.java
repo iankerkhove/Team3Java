5467d973c7213579657fb955f0c6d5869c4c73cb
Jonas DF <defrere.jonas@gmail.com>
2016-11-02 17:25:08 +0100

    remove string day (compatibility issues)

--- NMBSJava/src/gui/GUIDateFormat.java
+++ NMBSJava/src/gui/GUIDateFormat.java
@@ -8,15 +8,13 @@ import javax.swing.JFormattedTextField.AbstractFormatter;
 @SuppressWarnings("serial")
 public class GUIDateFormat extends AbstractFormatter {
 
-    private String datePattern = "E dd/MM/yyyy";
+    private String datePattern = "dd/MM/yyyy";
     private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
-
-    @Override
+    
     public Object stringToValue(String text) throws ParseException {
         return dateFormatter.parseObject(text);
     }
-
-    @Override
+    
     public String valueToString(Object value) throws ParseException {
         if (value != null) {
             Calendar cal = (Calendar) value;
