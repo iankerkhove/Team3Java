1f359103f3ff6d70efee96b2101fee389146c391
Jonas DF <defrere.jonas@gmail.com>
2016-11-04 19:09:20 +0100

    added getTime() and getDate() for optimal GUI usage

--- NMBSJava/src/gui/GUIDateFormat.java
+++ NMBSJava/src/gui/GUIDateFormat.java
@@ -2,26 +2,39 @@ package gui;
 
 import java.text.*;
 import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.JFormattedTextField.AbstractFormatter;
 
 @SuppressWarnings("serial")
 public class GUIDateFormat extends AbstractFormatter {
 
-    private String datePattern = "dd/MM/yyyy";
-    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
-    
-    public Object stringToValue(String text) throws ParseException {
-        return dateFormatter.parseObject(text);
-    }
-    
-    public String valueToString(Object value) throws ParseException {
-        if (value != null) {
-            Calendar cal = (Calendar) value;
-            return dateFormatter.format(cal.getTime());
-        }
+	private static String timePattern = "HH:mm";
+	private static String datePattern = "dd/MM/yyyy";
+	private static SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+	private static SimpleDateFormat timeFormatter = new SimpleDateFormat(timePattern);
 
-        return "";
-    }
+	public Object stringToValue(String text) throws ParseException {
+		return dateFormatter.parseObject(text);
+	}
+
+	public String valueToString(Object value) throws ParseException {
+		if (value != null) {
+			Calendar cal = (Calendar) value;
+			return dateFormatter.format(cal.getTime());
+		}
+
+		return "";
+	}
+
+	public static String getDate() {
+		String ss = dateFormatter.format(new Date());
+		return ss;
+	}
+
+	public static String getTime() {
+		String ss = timeFormatter.format(new Date());
+		return ss;
+	}
 
 }
