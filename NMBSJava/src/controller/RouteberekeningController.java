52570b716659e2fb638cd7863fe2978b33e60b5e
Jonas DF <defrere.jonas@gmail.com>
2016-11-04 16:06:41 +0100

    added controller for searching routes

---
+++ NMBSJava/src/controller/RouteberekeningController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import api.RouteberekeningAPI;
+import panels.RouteberekeningPanel;
+
+public class RouteberekeningController {
+	public static void startListening(RouteberekeningPanel route) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				route.getBtnZoek().addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						RouteberekeningAPI r;
+						String van = route.getTxtVan().getText();
+						String naar = route.getTxtNaar().getText();
+						String datum = route.getDatePicker().getJFormattedTextField().getText();
+						String tijd = route.getTimePicker().getText();
+						
+						if (!van.equals("") && !naar.equals("") && DateTimeConverter.checkTime(tijd) && DateTimeConverter.checkDate(datum)) {
+							
+							r = new RouteberekeningAPI(van, naar, DateTimeConverter.getEpoch(datum, tijd));
+							String ss = r.toString();
+							if (!ss.contains("null")) {
+								route.getLblResult().setText("<html>"
+										+ ss.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br/>")
+										+ "</html>");
+							} else
+								route.getLblResult().setText("Dit verzoek kon niet verwerkt worden.");
+						} else {
+							route.getLblResult().setText("Formulier werd niet correct ingevuld.");
+						}
+
+					}
+				});
+			}
+		});
+	}
+}
