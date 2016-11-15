0927815e4e04e6ddb5815fc54c545cf9f7b26c13
DESKTOP-2JH3AO2\geldh <geldhofremco@hotmail.com>
2016-11-15 16:26:08 +0100

    VerlengAbonnementPanel verbeterd, code volledig herstructureerd

--- NMBSJava/src/panels/VerlengAbonnementPanel.java
+++ NMBSJava/src/panels/VerlengAbonnementPanel.java
@@ -1,12 +1,15 @@
 package panels;
 
+import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.Frame;
 import java.awt.GridLayout;
 import java.util.Properties;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -19,7 +22,20 @@ import org.jdatepicker.impl.UtilDateModel;
 import gui.GUIDateFormat;
 
 public class VerlengAbonnementPanel extends JPanel {
-
+	
+	private JPanel titel;
+	private JPanel abonnementsNummer;
+	private JPanel klantenNummer;
+	private JPanel treinkaart;
+	private JPanel startdatum;
+	private JPanel duur;
+	private JPanel vervaldatum;
+	private JPanel klasse;
+	private JPanel traject;
+	private JPanel station1;
+	private JPanel station2;
+	private JPanel printEnValideer;
+	
 	private JLabel lblTitle;
 	private JLabel lblAbonnementsNummer;
 	private JLabel lblKlantenNummer;
@@ -28,6 +44,7 @@ public class VerlengAbonnementPanel extends JPanel {
 	private JLabel lblDuur;
 	private JLabel lblDuurResult;
 	private JLabel lblVervaldatum;
+	private JLabel lblVervaldatumResult;
 	private JLabel lblKlasse;
 	private JLabel lblVastTraject;
 	private JLabel lblTreinkaart;
@@ -35,10 +52,7 @@ public class VerlengAbonnementPanel extends JPanel {
 	private JLabel lblStation2;
 	private JLabel lblPrint;
 
-	
 	private JDatePickerImpl dteStartdatum;
-	
-	private JComboBox cbxTreinkaart;
 	
 	private JRadioButton rdbEersteKlasse;
 	private JRadioButton rdbTweedeKlasse;
@@ -53,45 +67,98 @@ public class VerlengAbonnementPanel extends JPanel {
 	private JButton btnMeerInfo;
 	private JButton btnValideer;
 
-	
 	private JTextField txtAbonnementsNummer;
 	private JTextField txtStation1;
 	private JTextField txtStation2;
 	
-	public VerlengAbonnementPanel() {
-		this.setLayout(new GridLayout(12, 3, 1, 1));
+	private JComboBox cbxTreinkaart;
+	private JComboBox cbxDuur;
 
+	public VerlengAbonnementPanel() {
+		titelPanel();
+		abonnementsNummerPanel();
+		klantenNummerPanel();
+		treinkaartPanel();
+		startdatumPanel();
+		duurPanel();
+		vervaldatumPanel();
+		trajectPanel();
+		klassePanel();
+		station1Panel();
+		station2Panel();
+		printEnValideerPanel();
+		fullPanel();
+	}
+	
+	private void fullPanel(){
+		this.setLayout(new GridLayout(13,1,1,1));
+		this.add(titel);
+		this.add(abonnementsNummer);
+		this.add(klantenNummer);
+		this.add(treinkaart);
+		this.add(startdatum);
+		this.add(duur);
+		this.add(vervaldatum);
+		this.add(traject);
+		this.add(klasse);
+		this.add(station1);
+		this.add(station2);
+		this.add(printEnValideer);
+	}
+	
+	private void titelPanel(){
+		titel=new JPanel();
+		titel.setLayout(new GridLayout(1, 1,1,1));
+		
 		lblTitle = new JLabel("Verleng Abonnement");
 		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
+		titel.add(lblTitle);
+	}
+	
+	private void abonnementsNummerPanel(){
+		abonnementsNummer = new JPanel();
+		abonnementsNummer.setLayout(new GridLayout(1,3,1,1));
 		
 		lblAbonnementsNummer = new JLabel("Abonnementsnummer: ");
 		txtAbonnementsNummer = new JTextField();
 		btnZoek = new JButton("Zoek");
+		
+		abonnementsNummer.add(lblAbonnementsNummer);
+		abonnementsNummer.add(txtAbonnementsNummer);
+		abonnementsNummer.add(btnZoek);
+	}
+	
+	private void klantenNummerPanel(){
+		klantenNummer = new JPanel();
+		klantenNummer.setLayout(new GridLayout(1,3,1,1));
+		
 		lblKlantenNummer = new JLabel("Klantennumer: ");
 		lblKLantenNummerResult = new JLabel("123456");
-		lblStartdatum = new JLabel("Startdatum: ");
-		
+		btnMeerInfo = new JButton("Meer info");
+
+		klantenNummer.add(lblKlantenNummer);
+		klantenNummer.add(lblKLantenNummerResult);
+		klantenNummer.add(btnMeerInfo);	
+	}
+	
+	private void treinkaartPanel(){
+		treinkaart = new JPanel();
+		treinkaart.setLayout(new GridLayout(1,2,1,1));
+
 		lblTreinkaart = new JLabel("Treinkaart: ");
 		String[] str = { "Trajecttreinkaart", "Halftijdstreinkaart", "Nettreinkaart", "Schooltreinkaart" };
 		cbxTreinkaart = new JComboBox(str);
 		
-		lblDuur = new JLabel("Duur: ");
-		lblDuurResult = new JLabel("'result'");
-		lblVervaldatum = new JLabel("'result'");
-		
-		lblKlasse = new JLabel("Klasse: ");
-		rdbEersteKlasse = new JRadioButton("1e Klas");
-		rdbTweedeKlasse = new JRadioButton("2e Klas");
-		
-		lblVastTraject = new JLabel("Vast traject: ");
-		rdbJa = new JRadioButton("Ja");
-		rdbNee=new JRadioButton("Nee");
-		
-		lblStation1 = new JLabel("Station 1: ");
-		lblStation2 = new JLabel("Station 2: ");
-		txtStation1 = new JTextField();
-		txtStation2 = new JTextField();
-		
+		treinkaart.add(lblTreinkaart);
+		treinkaart.add(cbxTreinkaart);
+	}
+	
+	private void startdatumPanel(){
+		startdatum = new JPanel();
+		startdatum.setLayout(new GridLayout(1,2,1,1));
+
+		lblStartdatum = new JLabel("Startdatum: ");
 		Properties properties = new Properties();
 		properties.put("text.today", "Today");
 		properties.put("text.month", "Month");
@@ -99,68 +166,150 @@ public class VerlengAbonnementPanel extends JPanel {
 		JDatePanelImpl datePanel = new JDatePanelImpl(new UtilDateModel(), properties);
 		dteStartdatum = new JDatePickerImpl(datePanel, new GUIDateFormat());
 		
+		startdatum.add(lblStartdatum);
+		startdatum.add(dteStartdatum);
+	}
+	
+	private void duurPanel(){
+		duur = new JPanel();
+		duur.setLayout(new GridLayout(1,2,1,1));
+
+		lblDuur = new JLabel("Duur: ");
+		String[] aantalMaanden = { "1 maand", "3 maanden", "12 maanden"};
+		cbxDuur = new JComboBox(aantalMaanden);
+		
+		duur.add(lblDuur);
+		duur.add(cbxDuur);
+	}
+	
+	private void vervaldatumPanel(){
+		vervaldatum = new JPanel();
+		vervaldatum.setLayout(new GridLayout(1,2,1,1));
+
+		lblVervaldatum = new JLabel("Vervaldatum: ");
+		lblVervaldatumResult = new JLabel(" ");
+		
+		vervaldatum.add(lblVervaldatum);
+		vervaldatum.add(lblVervaldatumResult);
+	}
+	
+	private void klassePanel(){
+		klasse = new JPanel();
+		klasse.setLayout(new GridLayout(1, 3,1,1));
+		
 		grpKlasses = new ButtonGroup();
 		grpKlasses.add(rdbEersteKlasse);
 		grpKlasses.add(rdbTweedeKlasse);
+		
+		lblKlasse = new JLabel("Klasse: ");
+		rdbEersteKlasse = new JRadioButton("1e Klas");
+		rdbTweedeKlasse = new JRadioButton("2e Klas");
+		
+		klasse.add(lblKlasse);
+		klasse.add(rdbEersteKlasse);
+		klasse.add(rdbTweedeKlasse);
+	}
+	
+	private void trajectPanel(){
+		traject = new JPanel();
+		traject.setLayout(new GridLayout(1, 3,1,1));
 		
 		grpJaNee = new ButtonGroup();
 		grpJaNee.add(rdbJa);
 		grpJaNee.add(rdbNee);
 		
+		lblVastTraject = new JLabel("Vast traject: ");
+		rdbJa = new JRadioButton("Ja");
+		rdbNee=new JRadioButton("Nee");
+		
+		traject.add(lblVastTraject);
+		traject.add(rdbJa);
+		traject.add(rdbNee);
+	}
+	
+	private void station1Panel(){
+		station1 = new JPanel();
+		station1.setLayout(new GridLayout(1,2,1,1));		
+		
+		lblStation1 = new JLabel("Station 1: ");
+		txtStation1 = new JTextField();
+		
+		station1.add(lblStation1);
+		station1.add(txtStation1);
+		
+	}
+	
+	private void station2Panel(){
+		station2 = new JPanel();
+		station2.setLayout(new GridLayout(1,2,1,1));
+		
+		lblStation2 = new JLabel("Station 2: ");
+		txtStation2 = new JTextField();
+		
+		station2.add(lblStation2);
+		station2.add(txtStation2);
+	}
+	
+	
+	private void printEnValideerPanel(){
+		printEnValideer = new JPanel();
+		printEnValideer.setLayout(new GridLayout(1, 3, 1, 1));
+		
 		btnPrint = new JButton("PRINT");
-		lblPrint = new JLabel("€0");
-		
-		btnMeerInfo = new JButton("Meer info");
-		
+		lblPrint = new JLabel("€0");		
 		btnValideer = new JButton("Valideer");
 		
-		this.add(lblTitle);
-		this.add(new JLabel());
-		this.add(new JLabel());
-		
-		
-		this.add(lblAbonnementsNummer);
-		this.add(txtAbonnementsNummer);
-		this.add(btnZoek);
-		
-		this.add(lblKlantenNummer);
-		this.add(lblKLantenNummerResult);
-		this.add(btnMeerInfo);
+		printEnValideer.add(btnPrint);
+		printEnValideer.add(lblPrint);
+		printEnValideer.add(btnValideer);
+	}
 
-		this.add(lblTreinkaart);
-		this.add(cbxTreinkaart);
-		this.add(new JLabel());
-		
-		this.add(lblStartdatum);
-		this.add(dteStartdatum);
-		this.add(new JLabel());
+	public JPanel getTitel() {
+		return titel;
+	}
 
-		this.add(lblDuur);
-		this.add(lblDuurResult);
-		this.add(lblVervaldatum);
-		
-		this.add(lblKlasse);
-		this.add(rdbEersteKlasse);
-		this.add(rdbTweedeKlasse);
-		
-		this.add(lblVastTraject);
-		this.add(rdbJa);
-		this.add(rdbNee);
-		this.add(new JLabel());
+	public JPanel getAbonnementsNummer() {
+		return abonnementsNummer;
+	}
 
-		this.add(lblStation1);
-		this.add(txtStation1);
-		this.add(new JLabel());
-		this.add(lblStation2);
-		this.add(txtStation2);
-		
-		this.add(btnPrint);
-		this.add(lblPrint);
-		this.add(new JLabel());
-		this.add(new JLabel());
-		this.add(new JLabel());
+	public JPanel getKlantenNummer() {
+		return klantenNummer;
+	}
 
-		this.add(btnValideer);
+	public JPanel getTreinkaart() {
+		return treinkaart;
+	}
+
+	public JPanel getStartdatum() {
+		return startdatum;
+	}
+
+	public JPanel getDuur() {
+		return duur;
+	}
+
+	public JPanel getVervaldatum() {
+		return vervaldatum;
+	}
+
+	public JPanel getKlasse() {
+		return klasse;
+	}
+
+	public JPanel getTraject() {
+		return traject;
+	}
+
+	public JPanel getStation1() {
+		return station1;
+	}
+
+	public JPanel getStation2() {
+		return station2;
+	}
+
+	public JPanel getPrintEnValideer() {
+		return printEnValideer;
 	}
 
 	public JLabel getLblTitle() {
@@ -195,6 +344,10 @@ public class VerlengAbonnementPanel extends JPanel {
 		return lblVervaldatum;
 	}
 
+	public JLabel getLblVervaldatumResult() {
+		return lblVervaldatumResult;
+	}
+
 	public JLabel getLblKlasse() {
 		return lblKlasse;
 	}
@@ -221,10 +374,6 @@ public class VerlengAbonnementPanel extends JPanel {
 
 	public JDatePickerImpl getDteStartdatum() {
 		return dteStartdatum;
-	}
-
-	public JComboBox getCbxTreinkaart() {
-		return cbxTreinkaart;
 	}
 
 	public JRadioButton getRdbEersteKlasse() {
@@ -278,6 +427,15 @@ public class VerlengAbonnementPanel extends JPanel {
 	public JTextField getTxtStation2() {
 		return txtStation2;
 	}
+
+	public JComboBox getCbxTreinkaart() {
+		return cbxTreinkaart;
+	}
+
+	public JComboBox getCbxDuur() {
+		return cbxDuur;
+	}
+
 	
 	
 
