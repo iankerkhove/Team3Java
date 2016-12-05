63de3cb7b98a1d7ac348d2789ca48f933251e806
ian <ian@10.3.210.181>
2016-12-05 20:40:32 +0100

    aanmaken verschillende tests + fout opgelost in koopbiljetcontroller

--- NMBSJava/src/controller/KoopBiljetController.java
+++ NMBSJava/src/controller/KoopBiljetController.java
@@ -3,16 +3,9 @@ package controller;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-//import model.Ticket;
 import panels.BiljetPanel;
 
 public class KoopBiljetController {
@@ -25,20 +18,22 @@ public class KoopBiljetController {
 	private static int typeKlasseSelectedMnem;
 	private static int vanID;
 	private static int naarID;
+	private static String prijs;
 
 	public static void startListening(BiljetPanel biljet) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				biljet.getBtnPrint().addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						van = biljet.getTxtVan().getText();
-						naar = biljet.getTxtNaar().getText();
+						van = (String) biljet.getTxtVan().getSelectedItem();
+						naar = (String) biljet.getTxtVan().getSelectedItem();
 						begindatum = biljet.getDteGaanDatum().getJFormattedTextField().getText();
 						einddatum = biljet.getDteTerugDatum().getJFormattedTextField().getText();
 						int typeHeenTerugSelectedMnem = biljet.getGrpHeenTerug().getSelection().getMnemonic();
 						String soortBiljet = biljet.getCboBiljet().getSelectedItem().toString();
 						typeKlasseSelectedMnem = biljet.getGrpKlasseTicket().getSelection().getMnemonic();
-						
+						prijs = biljet.getLblPrijs().getText();
+
 						if (!van.equals("") && !naar.equals("") && DateTimeConverter.checkDate(begindatum)
 								&& DateTimeConverter.checkDate(einddatum) && typeHeenTerugSelectedMnem != 0
 								&& typeKlasseSelectedMnem != 0 && !soortBiljet.equals(null)) {
@@ -47,6 +42,7 @@ public class KoopBiljetController {
 							if (temp == 200) {
 								System.out.println("Juist biljet");
 							} else {
+								System.err.println(routeID);
 								System.err.println("Fout in API, statuscode: " + temp);
 							}
 						} else {
@@ -55,155 +51,104 @@ public class KoopBiljetController {
 
 					}
 				});
-				biljet.getCboBiljet().addActionListener(new ActionListener(){
+				biljet.getCboBiljet().addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						if(biljet.getCboBiljet().getSelectedItem().equals("Standaardbiljet")){
+						if (biljet.getCboBiljet().getSelectedItem().equals("Standaardbiljet")) {
 							biljet.getDteTerugDatum().setEnabled(false);
-							//biljet.getLblPrijs().setText("€ 10");
-							if(biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							// biljet.getLblPrijs().setText("€ 10");
+							if (biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 15");
-							}
-							else if((biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							} else if ((biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 10");
 							}
-							if(biljet.getRdbKlasseVerhoging().isSelected()){
+							if (biljet.getRdbKlasseVerhoging().isSelected()) {
 								biljet.getLblPrijs().setText("€ 2");
 							}
 						}
-						if(biljet.getCboBiljet().getSelectedItem().equals("GoPass 1")){
-							//biljet.getLblPrijs().setText("€ 6");
-							if(biljet.getRdbHeenTerug().isSelected()&&(biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+						if (biljet.getCboBiljet().getSelectedItem().equals("GoPass 1")) {
+							// biljet.getLblPrijs().setText("€ 6");
+							if (biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 10");
-							}
-							else if((biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							} else if ((biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 6");
 							}
-							if(biljet.getRdbKlasseVerhoging().isSelected()){
+							if (biljet.getRdbKlasseVerhoging().isSelected()) {
 								biljet.getLblPrijs().setText("€ 1");
 							}
 						}
-						if(biljet.getCboBiljet().getSelectedItem().equals("Weekendbiljet")){
-							//biljet.getLblPrijs().setText("€ 12.5");
-							if(biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+						if (biljet.getCboBiljet().getSelectedItem().equals("Weekendbiljet")) {
+							// biljet.getLblPrijs().setText("€ 12.5");
+							if (biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 20");
-							}
-							else if((biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							} else if ((biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 12.5");
 							}
-							if(biljet.getRdbKlasseVerhoging().isSelected()){
+							if (biljet.getRdbKlasseVerhoging().isSelected()) {
 								biljet.getLblPrijs().setText("€ 2");
 							}
 						}
-						if(biljet.getCboBiljet().getSelectedItem().equals("Seniorenbiljet")){
-							//biljet.getLblPrijs().setText("€ 5");
-							if(biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+						if (biljet.getCboBiljet().getSelectedItem().equals("Seniorenbiljet")) {
+							// biljet.getLblPrijs().setText("€ 5");
+							if (biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 8");
-							}
-							else if((biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							} else if ((biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 5");
 							}
-							if(biljet.getRdbKlasseVerhoging().isSelected()){
+							if (biljet.getRdbKlasseVerhoging().isSelected()) {
 								biljet.getLblPrijs().setText("€ 1");
 							}
-						} 
-						if(biljet.getCboBiljet().getSelectedItem().equals("Biljet Kind")){
-							//biljet.getLblPrijs().setText("€ 5");
-							if(biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+						}
+						if (biljet.getCboBiljet().getSelectedItem().equals("Biljet Kind")) {
+							// biljet.getLblPrijs().setText("€ 5");
+							if (biljet.getRdbHeenTerug().isSelected() && (biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 8");
-							}
-							else if((biljet.getRdbEersteKlasse().isSelected()||biljet.getRdbTweedeKlasse().isSelected())){
+							} else if ((biljet.getRdbEersteKlasse().isSelected()
+									|| biljet.getRdbTweedeKlasse().isSelected())) {
 								biljet.getLblPrijs().setText("€ 5");
 							}
-							if(biljet.getRdbKlasseVerhoging().isSelected()){
+							if (biljet.getRdbKlasseVerhoging().isSelected()) {
 								biljet.getLblPrijs().setText("€ 0");
 							}
 						}
 						//
-						
+
 					}
-					
+
 				});
 			}
 		});
 	}
 
 	public static void readRouteID() {
-		
-		BufferedReader reader = null;
 		try {
 			readStationID();
-			URL url = new URL("http://nmbs-team.tk/api/route/" + vanID + "/" + naarID);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			JSONObject temp = new JSONObject(
+					URLCon.readUrl("http://nmbs-team.tk/api/route/" + vanID + "/" + naarID, "GET"));
+			System.err.println(temp.toString(4));
 
-			connection.setRequestMethod("GET");
-			connection.setRequestProperty("Authorization", "Bearer " + LoginController.getToken());
-			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer buffer = new StringBuffer();
-			int read;
-			char[] chars = new char[1024];
-			while ((read = reader.read(chars)) != -1)
-				buffer.append(chars, 0, read);
-
-			// JSONArray jsonTemp = new JSONArray(buffer.toString());
-
-			JSONObject temp = new JSONObject(buffer.toString());
-
-			int statusCode = temp.getInt("StatusCode");
-			if (statusCode == 404) {
-				createRoute();
+			if (temp.has("StatusCode")) {
+				int statusCode = temp.getInt("StatusCode");
+				if (statusCode == 404) {
+					createRoute();
+				}
 			}
-			// zien hoe de volledige json er uit ziet
-			// System.out.println(json.toString(4));
-			/*
-			 * String temp = ""; String route = van + " - " + naar; for (int i =
-			 * 0; i < jsonTemp.length(); i++) { temp +=
-			 * jsonTemp.getJSONObject(i).getJSONObject("DepartureStation").
-			 * getString("Name"); temp += " - "; temp +=
-			 * jsonTemp.getJSONObject(i).getJSONObject("ArrivalStation").
-			 * getString("Name");
-			 * 
-			 * if (route.equals(temp)) { routeID =
-			 * jsonTemp.getJSONObject(i).getInt("RouteID");
-			 * System.out.println(routeID); } else { temp = ""; readStationID();
-			 * } }
-			 */
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 
 	public static void readStationID() {
-
-		BufferedReader reader = null;
 		try {
-			URL url = new URL("http://nmbs-team.tk/api/station");
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-			connection.setRequestMethod("GET");
-			connection.setRequestProperty("Authorization", "Bearer " + LoginController.getToken());
-			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer buffer = new StringBuffer();
-			int read;
-			char[] chars = new char[1024];
-			while ((read = reader.read(chars)) != -1)
-				buffer.append(chars, 0, read);
-
-			JSONArray json = new JSONArray(buffer.toString());
-			// zien hoe de volledige json er uit ziet
+			JSONArray json = new JSONArray(URLCon.readUrl("http://nmbs-team.tk/api/station", "GET"));
 
 			boolean goTroughVan = false;
 			boolean goTroughNaar = false;
@@ -214,116 +159,57 @@ public class KoopBiljetController {
 
 				temp = json.getJSONObject(i).getString("Name");
 
-				if (van.equals(temp)) {
+				if (van.toUpperCase().equals(temp.toUpperCase())) {
 					vanID = json.getJSONObject(i).getInt("StationID");
 					goTroughVan = true;
 				}
 
-				if (naar.equals(temp)) {
+				if (naar.toUpperCase().equals(temp.toUpperCase())) {
 					naarID = json.getJSONObject(i).getInt("StationID");
 					goTroughNaar = true;
 				}
 			}
 
 			if (goTroughVan && goTroughNaar) {
-				//createRoute();
+				// createRoute();
 			} else {
 				System.err.println("Een van de stations werd niet gevonden.");
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 
 	public static int createRoute() {
-
-		BufferedReader reader = null;
 		try {
-			URL url = new URL(
-					"http://nmbs-team.tk/api/route/create?DepartureStationID=" + vanID + "&ArrivalStationID=" + naarID);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Authorization", "Bearer " + LoginController.getToken());
-			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer buffer = new StringBuffer();
-			int read;
-			char[] chars = new char[1024];
-			while ((read = reader.read(chars)) != -1)
-				buffer.append(chars, 0, read);
-
-			JSONObject temp = new JSONObject(buffer.toString());
-
+			JSONObject temp = new JSONObject(URLCon.readUrl(
+					"http://nmbs-team.tk/api/route/create?DepartureStationID=" + vanID + "&ArrivalStationID=" + naarID,
+					"POST"));
 			System.out.println(temp.toString(4));
-			//
-
 			int statusCode = temp.getInt("StatusCode");
 			if (statusCode == 200) {
 				routeID = temp.getInt("RouteID");
 			}
 			return statusCode;
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
 		}
-		// readRouteID();
-
 		return 500;
 	}
 
 	public static int createTicket() {
-
-		BufferedReader reader = null;
 		try {
-			URL url = new URL("http://nmbs-team.tk/api/ticket/create?RouteID=" + routeID + "&Date=" + begindatum
-					+ "&Price=10&ValidFrom=" + begindatum + "&ValidUntil=" + einddatum + "&ComfortClass="
-					+ typeKlasseSelectedMnem);
-
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Authorization", "Bearer " + LoginController.getToken());
-			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer buffer = new StringBuffer();
-			int read;
-			char[] chars = new char[1024];
-			while ((read = reader.read(chars)) != -1)
-				buffer.append(chars, 0, read);
-
-			JSONObject temp = new JSONObject(buffer.toString());
+			JSONObject temp = new JSONObject(
+					URLCon.readUrl(
+							"http://nmbs-team.tk/api/ticket/create?RouteID=" + routeID + "&Date=" + begindatum
+									+ "&TypeTicketID=1" + "&ValidFrom=" + begindatum + "&ValidUntil=" + einddatum,
+							"POST"));
 			System.out.println(temp.toString(4));
 			int statusCode = temp.getInt("StatusCode");
 			return statusCode;
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		return 500;
 	}
