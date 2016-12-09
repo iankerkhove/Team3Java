56a11ac88a057153dbc40a0402afe2a1bd614003
PudiPudi <ludovic.marchand@student.ehb.be>
2016-12-09 15:33:59 +0100

    urlCronWorker for mutliple apis; cleaning up code;

--- NMBSJava/src/controller/LoginController.java
+++ NMBSJava/src/controller/LoginController.java
@@ -12,60 +12,64 @@ import javax.swing.SwingWorker;
 
 import org.json.JSONObject;
 
+import controller.UrlConWorker.APIUrl;
 import controller.UrlConWorker.RequestType;
 import panels.LoginPanel;
 
-public class LoginController {
+public class LoginController
+{
 
 	private static int staffID;
 	private static String token;
 	private static int statuscode;
-	
+
 	private static UrlConWorker urlConWorker;
 
-	public static void login(LoginPanel l) {
+	public static void login(LoginPanel l)
+	{
 		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+			public void run()
+			{
 				l.getBtnLogin().addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
+					public void actionPerformed(ActionEvent e)
+					{
 						readUrl(l);
-						
+
 					}
 				});
 			}
 		});
 	}
-	
 
-
-	private static void readUrl(LoginPanel l) {
+	private static void readUrl(LoginPanel l)
+	{
 		String usrn = l.getTxtUsername().getText().replaceAll("&", "%26");
 		String password = l.getTxtPassword().getText().replaceAll("&", "%26");
 		// boolean chAdmin = l.getChAdmin().isSelected();
-		
+
 		HashMap<String, String> params = new HashMap<String, String>();
 		params.put("username", usrn);
 		params.put("password", password);
-		
-		urlConWorker = new UrlConWorker("staff/login", RequestType.POST, params);
-		
+
+		urlConWorker = new UrlConWorker(APIUrl.G3, "staff/login", RequestType.POST, params);
+
 		urlConWorker.addPropertyChangeListener(new PropertyChangeListener() {
 
 			@Override
-			public void propertyChange(PropertyChangeEvent evt) {
-				
-				if("state".equals(evt.getPropertyName())
-						&& SwingWorker.StateValue.DONE.equals(evt.getNewValue())) {
-					
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+
+				if ("state".equals(evt.getPropertyName()) && SwingWorker.StateValue.DONE.equals(evt.getNewValue())) {
+
 					try {
 						JSONObject json = urlConWorker.get();
-						
+
 						statuscode = json.getInt("StatusCode");
 						if (statuscode == 200) {
 							token = json.getString("Api_token");
 							staffID = json.getInt("StaffID");
 						}
-						
+
 						if (statuscode == 200) {
 							l.getLblResult().setText("");
 							GUIController.getFrame().getContentPane().removeAll();
@@ -73,39 +77,43 @@ public class LoginController {
 							CacheExistingSations.cache();
 							CacheTicketTypes.cache();
 							CachePassTypes.cache();
-						} else {
+						}
+						else {
 							l.getLblResult().setText("Fout, probeer opnieuw!");
 							l.getTxtUsername().setText("");
 							l.getTxtPassword().setText("");
 						}
-					} catch (InterruptedException | ExecutionException e) {
+					}
+					catch (InterruptedException | ExecutionException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
-				
-				
-				
+
 			}
-			
+
 		});
-		
+
 		urlConWorker.execute();
 	}
 
-	public static int getStaffID() {
+	public static int getStaffID()
+	{
 		return staffID;
 	}
 
-	public static String getToken() {
+	public static String getToken()
+	{
 		return token;
 	}
 
-	public static int getStatuscode() {
+	public static int getStatuscode()
+	{
 		return statuscode;
 	}
 
-	public static void clearCreds() {
+	public static void clearCreds()
+	{
 		token = "";
 		staffID = 0;
 	}
