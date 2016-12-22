15f7048f4e0ae0b42d6a06557bbd752cd673d5ba
ian <ian@HPIan>
2016-12-22 22:26:12 +0100

    create init database

--- NMBSJava/src/dao/InitDatabase.java
+++ NMBSJava/src/dao/InitDatabase.java
@@ -1,8 +1,21 @@
 package dao;
 
-import dao.AddressDAO;
 public class InitDatabase {
 	public InitDatabase() {
 		AddressDAO.createTable();
+		CustomerDAO.createTable();
+		DiscountDAO.createTable();
+		LineDAO.createTable();
+		LostObjectDAO.createTable();
+		PassDAO.createTable();
+		RailCardDAO.createTable();
+		ReservationDAO.createTable();
+		RouteDAO.createTable();
+		StaffDAO.createTable();
+		StationDAO.createTable();
+		SubscriptionDAO.createTable();
+		TicketDAO.createTable();
+		TypePassDAO.createTable();
+		TypeTicketDAO.createTable();
 	}
 }
