057bcaa94617b6b753b2950f41f20e1999800c91
PudiPudi <ludovic.marchand@student.ehb.be>
2016-12-23 23:04:32 +0100

    SyncStation volledig werkend; aanpassingen maasupdate (nu wel werkend)

--- NMBSJava/src/services/SyncAllRunnable.java
+++ NMBSJava/src/services/SyncAllRunnable.java
@@ -12,7 +12,10 @@ public class SyncAllRunnable implements Runnable
     public SyncAllRunnable()
     {
     	workQueue = new LinkedTransferQueue<Runnable>();
-        workQueue.add(new SyncAddressRunnable());
+        
+    	workQueue.add(new SyncAddressRunnable());
+    	workQueue.add(new SyncStationRunnable());
+        
     }
     
 	@Override
