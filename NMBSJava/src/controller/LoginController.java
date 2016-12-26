5efedcd0c19389370780855ba246d0320375ef68
Jonas DF <defrere.jonas@gmail.com>
2016-12-26 15:44:57 +0100

    Added offline option to login, added security for rights

--- NMBSJava/src/controller/LoginController.java
+++ NMBSJava/src/controller/LoginController.java
@@ -15,35 +15,73 @@ import org.json.JSONObject;
 
 import controller.UrlConWorker.APIUrl;
 import controller.UrlConWorker.RequestType;
+import dao.StaffDAO;
+import model.Staff;
 import panels.LoginPanel;
+import services.JBcryptVerifier;
 
-public class LoginController
-{
+public class LoginController {
 
 	private static UUID staffID;
 	private static String token;
-	private static int statuscode;
+	private static int rechten;
 
 	private static UrlConWorker urlConWorker;
 
-	public static void login(LoginPanel l)
-	{
+	public static void login(LoginPanel l) {
 		EventQueue.invokeLater(new Runnable() {
-			public void run()
-			{
+			public void run() {
 				l.getBtnLogin().addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e)
-					{
-						readUrl(l);
-
+					public void actionPerformed(ActionEvent e) {
+						verify(l);
 					}
 				});
 			}
 		});
 	}
 
-	private static void readUrl(LoginPanel l)
-	{
+	private static void verify(LoginPanel l) {
+		String usrn = l.getTxtUsername().getText().replaceAll("&", "%26");
+		String password = l.getTxtPassword().getText().replaceAll("&", "%26");
+		boolean chAdmin = l.getChAdmin().isSelected();
+
+		StaffDAO handler = new StaffDAO();
+		Staff s = handler.selectOneOnUsername(usrn);
+		if (s != null) {
+			if (JBcryptVerifier.checkPassword(password, s.getPassword())){
+				if(chAdmin){
+					if (s.getRights() == 1) {
+						rechten = 1;
+						token = s.getApiToken();
+						staffID = s.getStaffID();
+						l.getLblResult().setText("");
+						GUIController.getFrame().getContentPane().removeAll();
+						GUIController.showApp();
+					} else {
+						l.getLblResult().setText("Geen admin rechten, probeer opnieuw!");
+					}
+				} else {
+					rechten = 0;
+					token = s.getApiToken();
+					staffID = s.getStaffID();
+					l.getLblResult().setText("");
+					GUIController.getFrame().getContentPane().removeAll();
+					GUIController.showApp();
+				}
+			} else {
+				l.getLblResult().setText("Fout password, probeer opnieuw!");
+				l.getTxtPassword().setText("");
+			}
+		} else {
+			l.getLblResult().setText("User ongeldig, probeer opnieuw!");
+			l.getTxtUsername().setText("");
+			l.getTxtPassword().setText("");
+		}
+	}
+
+	@SuppressWarnings("unused")
+	@Deprecated
+	private static void readUrl(LoginPanel l) {
 		String usrn = l.getTxtUsername().getText().replaceAll("&", "%26");
 		String password = l.getTxtPassword().getText().replaceAll("&", "%26");
 		// boolean chAdmin = l.getChAdmin().isSelected();
@@ -57,15 +95,14 @@ public class LoginController
 		urlConWorker.addPropertyChangeListener(new PropertyChangeListener() {
 
 			@Override
-			public void propertyChange(PropertyChangeEvent evt)
-			{
+			public void propertyChange(PropertyChangeEvent evt) {
 
 				if ("state".equals(evt.getPropertyName()) && SwingWorker.StateValue.DONE.equals(evt.getNewValue())) {
 
 					try {
 						JSONObject json = urlConWorker.get();
 
-						statuscode = json.getInt("StatusCode");
+						int statuscode = json.getInt("StatusCode");
 						if (statuscode == 200) {
 							token = json.getString("Api_token");
 							staffID = UUID.fromString(json.getString("StaffID"));
@@ -75,14 +112,12 @@ public class LoginController
 							l.getLblResult().setText("");
 							GUIController.getFrame().getContentPane().removeAll();
 							GUIController.showApp();
-						}
-						else {
+						} else {
 							l.getLblResult().setText("Fout, probeer opnieuw!");
 							l.getTxtUsername().setText("");
 							l.getTxtPassword().setText("");
 						}
-					}
-					catch (InterruptedException | ExecutionException e) {
+					} catch (InterruptedException | ExecutionException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
@@ -95,24 +130,20 @@ public class LoginController
 		urlConWorker.execute();
 	}
 
-	public static UUID getStaffID()
-	{
+	public static UUID getStaffID() {
 		return staffID;
 	}
 
-	public static String getToken()
-	{
+	public static String getToken() {
 		return token;
 	}
 
-	public static int getStatuscode()
-	{
-		return statuscode;
-	}
-
-	public static void clearCreds()
-	{
+	public static void clearCreds() {
 		token = "";
 		staffID = UUID.randomUUID();
+	}
+
+	public static int getRechten() {
+		return rechten;
 	}
 }
