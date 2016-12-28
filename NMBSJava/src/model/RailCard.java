eb64ca1e85914172b5de586bbc59eb0042ca50b0
PudiPudi <ludovic.marchand@student.ehb.be>
2016-12-28 04:20:40 +0100

    hash & equals (zonder lastUpdated) voor alle model klasses

--- NMBSJava/src/model/RailCard.java
+++ NMBSJava/src/model/RailCard.java
@@ -69,5 +69,35 @@ public class RailCard
 	{
 		this.lastUpdated = Instant.now().getEpochSecond();
 	}
+
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((railCardID == null) ? 0 : railCardID.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RailCard other = (RailCard) obj;
+		if (railCardID == null) {
+			if (other.railCardID != null)
+				return false;
+		}
+		else if (!railCardID.equals(other.railCardID))
+			return false;
+		return true;
+	}
 	
 }
--- NMBSJava/src/model/Staff.java
+++ NMBSJava/src/model/Staff.java
@@ -94,5 +94,62 @@ public class Staff extends Person
 	{
 		this.apiToken = apiToken;
 	}
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((apiToken == null) ? 0 : apiToken.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + rights;
+		result = prime * result + ((staffID == null) ? 0 : staffID.hashCode());
+		result = prime * result + ((stationID == null) ? 0 : stationID.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Staff other = (Staff) obj;
+		if (apiToken == null) {
+			if (other.apiToken != null)
+				return false;
+		}
+		else if (!apiToken.equals(other.apiToken))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		}
+		else if (!password.equals(other.password))
+			return false;
+		if (rights != other.rights)
+			return false;
+		if (staffID == null) {
+			if (other.staffID != null)
+				return false;
+		}
+		else if (!staffID.equals(other.staffID))
+			return false;
+		if (stationID == null) {
+			if (other.stationID != null)
+				return false;
+		}
+		else if (!stationID.equals(other.stationID))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		}
+		else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
 
 }
