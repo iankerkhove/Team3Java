e98c1cf7bfd725539b430b44a42c346f6be6676e
PudiPudi <ludovic.marchand@student.ehb.be>
2016-12-28 14:53:48 +0100

    Downlaod mainDB from online backup;

--- NMBSJava/src/services/InitDatabaseRunnable.java
+++ NMBSJava/src/services/InitDatabaseRunnable.java
@@ -1,5 +1,10 @@
 package services;
 
+import java.util.HashMap;
+
+import controller.APIController;
+import controller.APIController.APIUrl;
+import controller.APIController.RequestType;
 import dao.InitDatabase;
 import model.SettingsSingleton;
 
@@ -9,12 +14,28 @@ public class InitDatabaseRunnable implements Runnable
 	@Override
 	public void run()
 	{
-		InitDatabase.init();
-		
-		SettingsSingleton settings = SettingsSingleton.getSettings();
-		settings.setFirstTime("True");
-		
-		System.out.println("Databases-setted");
+
+		try {
+			HashMap<String, String> params = new HashMap<String, String>();
+			
+			params.put("TargetFilePath", "localDB.db");
+			
+			APIController g3API = new APIController(APIUrl.G3, "mixed/getDatabase", RequestType.GETFILE, params);
+			g3API.getJsonResult();
+			
+		}
+		catch (Exception e) {
+			
+			System.out.println("Download DB failed, initializing localDB");
+			
+			InitDatabase.init();
+			
+			SettingsSingleton settings = SettingsSingleton.getSettings();
+			settings.setFirstTime(true);
+			
+			System.out.println("Databases-setted");
+		}
+
 	}
 
 }
